38523d205dfe1a1fb5e8565f010c5b64ec251916
erfan <erfanmhdipoor@gmail.com>
2023-04-15 11:11:48 +0430

    remove

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [products,setProducts]=useState({
+    products:[
+      {title:"book1",price:35},
+      {title:"book2",price:30},
+      {title:"book3",price:25},
+    ]
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div>{products.products[0].title}</div>
     </div>
   );
 }
